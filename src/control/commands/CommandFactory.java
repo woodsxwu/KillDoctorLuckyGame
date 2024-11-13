521e94ad95902ee06369a5e8342d0e2e61f195a2
Zhixiao Wu <woodsxwu@gmail.com>
2024-11-12 21:05:52 -0800

    final style checking

--- src/control/commands/CommandFactory.java
+++ src/control/commands/CommandFactory.java
@@ -4,5 +4,11 @@ package control.commands;
  * Factory for creating commands.
  */
 public interface CommandFactory {
+  /**
+   * Creates a new command from the given arguments.
+   * 
+   * @param args the arguments to create the command from
+   * @return the new command
+   */
   GameCommand create(String[] args);
 }
